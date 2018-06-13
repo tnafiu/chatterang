bde57869cbe5e15ffadbde1b53eaae438ad6bc7f
Toluwa Nafiu <nafiutc@rose-hulman.edu>
2018-06-13 14:44:32 -0400

    Style MessageForm

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { StyleSheet, css } from 'aphrodite'
 
 class MessageForm extends Component {
   state = {
@@ -8,37 +9,52 @@ class MessageForm extends Component {
   handleSubmit = (ev) => {
     ev.preventDefault()
     this.props.addMessage(this.state.body)
-    this.setState({body: ''})
+    this.setState({ body: '' })
   }
 
   handleChange = (ev) => {
-    this.setState({body: ev.target.value})
+    this.setState({ body: ev.target.value })
   }
 
   render() {
     return (
       <form
-        className="MessageForm"
+        className={`MessageForm ${css(styles.form)}`}
         onSubmit={this.handleSubmit}
-        style = {styles.messageForm}
       >
+        <div className={css(styles.icon)}>
+          <i className="fas fa-comment-alt"></i>
+        </div>
         <input
           type="text"
           name="body"
           placeholder="Type a message..."
           value={this.state.body}
           onChange={this.handleChange}
-          style={styles.input}
           autoFocus
+          className={css(styles.input)}
         />
-        <button type="submit" style={styles.button}>Send</button>
+        <button type="submit" className={css(styles.button)}>
+          <i className="far fa-paper-plane" title="Send"></i>
+        </button>
       </form>
     )
   }
 }
 
-const styles = {
-  messageForm:{
+const styles = StyleSheet.create({
+  form: {
+    backgroundColor: 'white',
+    height: '3rem',
+    display: 'flex',
+    alignItems: 'stretch',
+    border: '2px solid #999',
+    borderRadius: '0.5rem',
+    margin: '0.25rem',
+    padding: 0,
+  },
+
+  icon: {
     display: 'flex',
     borderRadius: '0.5rem',
     alignItems: 'center',
@@ -47,14 +63,17 @@ const styles = {
     padding: '0 0.5rem',
     fontSize: '1.2rem',
   },
+
   input: {
-    flex: '1',
+    flex: 1,
     fontSize: '1.2rem',
-    border: '0',
+    border: 0,
+
+    ':focus': {
+      outline: 0,
+    },
   },
-  inputFocus: {
-    outline: '0',
-  },
+
   button: {
     fontSize: '1.5rem',
     backgroundColor: '#1A8FE3',
@@ -65,6 +84,6 @@ const styles = {
     borderBottomRightRadius: '0.5rem',
     border: '1px solid white',
   },
-}
+})
 
 export default MessageForm
